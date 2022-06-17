9d480088aa617358725754c6d6c1d110504ecf92
diogoizzo <izzoadv@gmail.com>
2022-06-17 11:10:06 -0300

    tentativa de corrigir o ícone do whatsapp no servidor

--- components/HeaderBanner.jsx
+++ components/HeaderBanner.jsx
@@ -11,7 +11,7 @@ export default function HeaderBanner() {
                     <Image alt="ícone do skype" src="/icons/Skype.svg" layout="fill"/>
                 </a>
                 <a href="#" className="relative h-7 w-7 mr-6">
-                    <Image alt="ícone do whatsapp" src="/icons/Whatsapp.svg" layout="fill"/>
+                    <Image alt="ícone do whatsapp" src="/icons/whatsapp.svg" layout="fill"/>
                 </a>
                 <a href="#" className="relative h-7 w-7">
                     <Image alt="ícone do linkedin" src="/icons/Linkedin.svg" layout="fill"/>
