2af9e9de64c5a57d13d6b5224ce0ba866b58c0d6
diogoizzo <izzoadv@gmail.com>
2022-06-17 14:07:24 -0300

    Alterada prioridade das bandeiras

--- components/PortosPais.jsx
+++ components/PortosPais.jsx
@@ -6,7 +6,7 @@ export default function PortosPais({bandeira, pais, portos}) {
             <div className={'flex items-center space-x-5'}>
                 <div className={'w-[181px] flex flex-col'}>
                     <Image src={bandeira} alt={`bandeira do ${pais}`}
-                           height={105} width={181} layout={'fixed'}/>
+                           height={105} width={181} layout={'fixed'} priority/>
                     <h4 className={'mt-2 text-lg text-center'}>{pais}</h4>
                 </div>
                 <p className={'-mt-8'}>
