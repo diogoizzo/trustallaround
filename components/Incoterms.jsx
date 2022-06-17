e90f718649ac76661e49f98694ccdc16154b1409
diogoizzo <izzoadv@gmail.com>
2022-06-17 14:01:00 -0300

    Alterada a margem bottom do component incoterms

--- components/Incoterms.jsx
+++ components/Incoterms.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Incoterms() {
     return (
-        <div className={'mt-8'}>
+        <div className={'my-8'}>
             <h1 className={'text-5xl font-semibold text-brand-orange mb-8 ml-12'}>Incoterms</h1>
             <Image src={'/images/incotermsTable.png'} alt={'tabela com os tipos de incoterms'} width={1557} height={771}
                    layout={"intrinsic"}/>
