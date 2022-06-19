7e1f79d567b7b9e812dd83f95ae0b432dd17dff6
diogoizzo <izzoadv@gmail.com>
2022-06-19 16:24:18 -0300

    Ajuste de resposividade da página inicial

--- components/HeaderBanner.jsx
+++ components/HeaderBanner.jsx
@@ -1,22 +1,35 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function HeaderBanner() {
     return (
         <div className="bg-[#808080] h-10 ">
-            <div className="h-full flex px-[6%] justify-end items-center shrink">
-                <a href="https://www.instagram.com/" className="relative h-7 w-7 mr-6">
-                    <Image className="fill-white" alt="ícone do instagram" src="/icons/Instagram.svg" layout="fill"/>
+            <div className="h-full flex px-[6%] justify-center sm:justify-end items-center shrink">
+                <a
+                    href="https://www.instagram.com/"
+                    className="relative h-7 w-7 mr-6"
+                >
+                    <Image
+                        className="fill-white"
+                        alt="ícone do instagram"
+                        src="/icons/Instagram.svg"
+                        layout="fill"
+                    />
                 </a>
                 <a href="#" className="relative h-7 w-7 mr-6">
-                    <Image alt="ícone do skype" src="/icons/Skype.svg" layout="fill"/>
-                </a>
-                <a href="#" className="relative h-7 w-7 mr-6">
-                    <Image alt="ícone do whatsapp" src="/icons/whatsapp.svg" layout="fill"/>
+                    <Image
+                        alt="ícone do whatsapp"
+                        src="/icons/whatsapp.svg"
+                        layout="fill"
+                    />
                 </a>
                 <a href="#" className="relative h-7 w-7">
-                    <Image alt="ícone do linkedin" src="/icons/Linkedin.svg" layout="fill"/>
+                    <Image
+                        alt="ícone do linkedin"
+                        src="/icons/Linkedin.svg"
+                        layout="fill"
+                    />
                 </a>
             </div>
         </div>
-    )
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,94 @@
 import Slider from "../components/Slider";
 import Image from "next/image";
-import {useTranslation} from "next-export-i18n";
+import { useTranslation } from "next-export-i18n";
 
 export default function Home() {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     return (
         <main>
-            <Slider/>
+            <Slider />
             <div className="grid gap-5 grid-cols-1 lg:grid-cols-3 px-[6%] my-12">
-                <div className="flex justify-center max-w-[420px] mx-auto items-center space-x-9">
-                <span className="mb-4">
-                    <Image alt="nossos valores" src='/images/valores.png' width={144} height={143} layout={"fixed"}/>
-                </span>
-                    <span>
-                    <Image alt="seta" src='/images/seta.png' width={28} height={96} layout={"fixed"}/>
-                </span>
+                <div className="flex flex-col sm:flex-row justify-center max-w-[420px] mx-auto items-center space-x-9">
+                    <span className="mb-4">
+                        <Image
+                            alt="nossos valores"
+                            src="/images/valores.png"
+                            width={144}
+                            height={143}
+                            layout={"fixed"}
+                        />
+                    </span>
+                    <span className={"hidden sm:block"}>
+                        <Image
+                            alt="seta"
+                            src="/images/seta.png"
+                            width={28}
+                            height={96}
+                            layout={"fixed"}
+                        />
+                    </span>
 
                     <div>
                         <ul className="list-disc ml-4 text-brand-blue text-lg">
-                            <li>{t('bullets.valores.seguranca')}</li>
-                            <li>{t('bullets.valores.confianca')}</li>
-                            <li>{t('bullets.valores.comprometimento')}</li>
-                            <li>{t('bullets.valores.inteligencia')}</li>
-                            <li>{t('bullets.valores.singularidade')}</li>
+                            <li>{t("bullets.valores.seguranca")}</li>
+                            <li>{t("bullets.valores.confianca")}</li>
+                            <li>{t("bullets.valores.comprometimento")}</li>
+                            <li>{t("bullets.valores.inteligencia")}</li>
+                            <li>{t("bullets.valores.singularidade")}</li>
                         </ul>
                     </div>
                 </div>
-                <div className="flex max-w-[420px] mx-auto items-center space-x-9">
-                <span>
-                    <Image alt="nossa visão" src='/images/visao.png' width={144} height={143} layout={"fixed"}/>
-                </span>
+                <div className="flex max-w-[420px] flex-col sm:flex-row mx-auto items-center space-x-9">
                     <span>
-                    <Image alt="seta" src='/images/seta.png' width={28} height={96} layout={"fixed"}/>
-                </span>
+                        <Image
+                            alt="nossa visão"
+                            src="/images/visao.png"
+                            width={144}
+                            height={143}
+                            layout={"fixed"}
+                        />
+                    </span>
+                    <span className="hidden sm:block">
+                        <Image
+                            alt="seta"
+                            src="/images/seta.png"
+                            width={28}
+                            height={96}
+                            layout={"fixed"}
+                        />
+                    </span>
                     <div>
-                        <p className="text-brand-blue text-lg">{t('bullets.visao')}</p>
+                        <p className="text-brand-blue text-lg">
+                            {t("bullets.visao")}
+                        </p>
                     </div>
                 </div>
-                <div className="flex max-w-[420px] mx-auto items-center space-x-9">
-                <span>
-                    <Image alt="nossa missão" src='/images/missao.png' width={144} height={143} layout={"fixed"}/>
-                </span>
+                <div className="flex max-w-[420px] flex-col sm:flex-row mx-auto items-center space-x-9">
                     <span>
-                    <Image alt="seta" src='/images/seta.png' width={28} height={96} layout={"fixed"}/>
-                </span>
+                        <Image
+                            alt="nossa missão"
+                            src="/images/missao.png"
+                            width={144}
+                            height={143}
+                            layout={"fixed"}
+                        />
+                    </span>
+                    <span className="hidden sm:block">
+                        <Image
+                            alt="seta"
+                            src="/images/seta.png"
+                            width={28}
+                            height={96}
+                            layout={"fixed"}
+                        />
+                    </span>
                     <div>
-                        <p className="text-brand-blue text-lg">{t('bullets.missao')}</p>
+                        <p className="text-brand-blue text-lg">
+                            {t("bullets.missao")}
+                        </p>
                     </div>
                 </div>
             </div>
         </main>
-
-
-    )
+    );
 }
