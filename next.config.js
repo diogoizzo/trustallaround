326540781d0d38d3eba93ada5c485e31650260f9
diogoizzo <izzoadv@gmail.com>
2022-07-28 19:06:06 -0300

    ajustando exportpaths

--- next.config.js
+++ next.config.js
@@ -6,14 +6,24 @@ const nextConfig = {
         { dev, dir, outDir, distDir, buildId }
     ) {
         return {
-            "/": { page: "/" },
+            "/": { page: "/", query: { lang: "pt" } },
+            "/sobre": { page: "/", query: { lang: "pt" } },
+            "/servicos": { page: "/servicos", query: { lang: "pt" } },
+            "/ferramentas": { page: "/ferramentas", query: { lang: "pt" } },
+            "/portos": { page: "/portos", query: { lang: "pt" } },
+            "/contato": { page: "/contato", query: { lang: "pt" } },
             "/en": { page: "/", query: { lang: "en" } },
-            "/es": { page: "/", query: { lang: "es" } },
             "/en/about": { page: "/sobre", query: { lang: "en" } },
             "/en/services": { page: "/servicos", query: { lang: "en" } },
             "/en/tools": { page: "/ferramentas", query: { lang: "en" } },
-            "/en/harbours": { page: "/ferramentas", query: { lang: "en" } },
-            "/en/contact": { page: "/ferramentas", query: { lang: "en" } },
+            "/en/ports": { page: "/portos", query: { lang: "en" } },
+            "/en/contact": { page: "/contato", query: { lang: "en" } },
+            "/es": { page: "/", query: { lang: "es" } },
+            "/es/nosostros": { page: "/sobre", query: { lang: "es" } },
+            "/es/servicios": { page: "/servicos", query: { lang: "es" } },
+            "/es/herramientas": { page: "/ferramentas", query: { lang: "es" } },
+            "/es/puertos": { page: "/portos", query: { lang: "es" } },
+            "/es/contacto": { page: "/contato", query: { lang: "es" } },
         };
     },
     experimental: {
