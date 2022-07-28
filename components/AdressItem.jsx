e6a30ec3ab9dbd24ba270a480ba0f77b46c45db8
diogoizzo <izzoadv@gmail.com>
2022-07-28 20:54:29 -0300

    Ajuste para os icones de contato não apertarem no mobile

--- components/AdressItem.jsx
+++ components/AdressItem.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function AdressItem({ src, alt, text }) {
     return (
         <div className={"flex mt-1"}>
-            <span className={"inline-flex justify-center items-center"}>
+            <span
+                className={"inline-flex justify-center items-center shrink-0"}
+            >
                 <Image
                     src={src}
                     alt={alt}
