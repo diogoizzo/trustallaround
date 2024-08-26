9ee1c1348decde9caa1252be4debf636de596e7e
diogoizzo <dizzodev@gmail.com>
2024-08-26 10:20:46 -0300

    pagina de servicos finaizada

--- pages/servicos/china/index.js
+++ pages/servicos/china/index.js
@@ -1,35 +1,10 @@
 import { useLanguageQuery, useTranslation } from "next-export-i18n";
-import { useEffect, useState } from "react";
 import NavServicos from "../../../components/NavServicos";
 import Image from "next/image";
 
 export default function Servicos() {
     const { t } = useTranslation();
     const [query] = useLanguageQuery();
-    // const [china, setChina] = useState({
-    //     p1: t("servicos.content.tab1.p1"),
-    //     p2: t("servicos.content.tab1.p2"),
-    //     li1: t("servicos.content.tab1.list.li1"),
-    //     li2: t("servicos.content.tab1.list.li2"),
-    //     li3: t("servicos.content.tab1.list.li3"),
-    //     li4: t("servicos.content.tab1.list.li4"),
-    //     li5: t("servicos.content.tab1.list.li5"),
-    //     li6: t("servicos.content.tab1.list.li6"),
-    // });
-
-    // useEffect(() => {
-    //     setServico({
-    //         p1: t("servicos.content.tab1.p1"),
-    //         p2: t("servicos.content.tab1.p2"),
-    //         li1: t("servicos.content.tab1.list.li1"),
-    //         li2: t("servicos.content.tab1.list.li2"),
-    //         li3: t("servicos.content.tab1.list.li3"),
-    //         li4: t("servicos.content.tab1.list.li4"),
-    //         li5: t("servicos.content.tab1.list.li5"),
-    //         li6: t("servicos.content.tab1.list.li6"),
-    //     });
-    // }, [query]);
-
     return (
         <div className="w-full overflow-hidden">
             <div
@@ -48,21 +23,41 @@ export default function Servicos() {
                                     "border-l-4 border-brand-orange pl-8 lg:pl-12 text-lg md:text-xl lg:text-2xl font-normal mt-5 lg:mt-12 "
                                 }
                             >
-                                <p>
-                                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                                </p>
-                                <p className="mt-3">xxxxxx</p>
-                            </div>
-                            <div className="float-right w-1/2 mt-10 mb-10 ml-4">
-                                <Image
-                                    src={"/images/china-container.jpg"}
-                                    alt={
-                                        "imagem mostrando um container vindo da china com vários produtos"
-                                    }
-                                    width={738}
-                                    height={335}
-                                    layout={"responsive"}
-                                />
+                                <p
+                                    className=""
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab5.p1"),
+                                    }}
+                                ></p>
+                                <div className="float-right w-1/2 mt-20 mb-10 ml-4">
+                                    <Image
+                                        src={"/images/china-container.jpg"}
+                                        alt={
+                                            "imagem mostrando um container vindo da china com vários produtos"
+                                        }
+                                        width={738}
+                                        height={335}
+                                        layout={"responsive"}
+                                    />
+                                </div>
+                                <p
+                                    className="mt-4 lg:mt-8"
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab5.p2"),
+                                    }}
+                                ></p>
+                                <p
+                                    className="mt-4 lg:mt-8"
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab5.p3"),
+                                    }}
+                                ></p>
+                                <p
+                                    className="mt-4 lg:mt-8 mb-16"
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab5.p4"),
+                                    }}
+                                ></p>
                             </div>
                         </div>
                     </div>
--- pages/servicos/seguro/index.js
+++ pages/servicos/seguro/index.js
@@ -1,35 +1,11 @@
 import { useLanguageQuery, useTranslation } from "next-export-i18n";
-import { useEffect, useState } from "react";
 import NavServicos from "../../../components/NavServicos";
 import Image from "next/image";
+import NextLink from "../../../components/NextLink";
 
 export default function Servicos() {
     const { t } = useTranslation();
     const [query] = useLanguageQuery();
-    // const [china, setChina] = useState({
-    //     p1: t("servicos.content.tab1.p1"),
-    //     p2: t("servicos.content.tab1.p2"),
-    //     li1: t("servicos.content.tab1.list.li1"),
-    //     li2: t("servicos.content.tab1.list.li2"),
-    //     li3: t("servicos.content.tab1.list.li3"),
-    //     li4: t("servicos.content.tab1.list.li4"),
-    //     li5: t("servicos.content.tab1.list.li5"),
-    //     li6: t("servicos.content.tab1.list.li6"),
-    // });
-
-    // useEffect(() => {
-    //     setServico({
-    //         p1: t("servicos.content.tab1.p1"),
-    //         p2: t("servicos.content.tab1.p2"),
-    //         li1: t("servicos.content.tab1.list.li1"),
-    //         li2: t("servicos.content.tab1.list.li2"),
-    //         li3: t("servicos.content.tab1.list.li3"),
-    //         li4: t("servicos.content.tab1.list.li4"),
-    //         li5: t("servicos.content.tab1.list.li5"),
-    //         li6: t("servicos.content.tab1.list.li6"),
-    //     });
-    // }, [query]);
-
     return (
         <div className="w-full overflow-hidden">
             <div
@@ -42,7 +18,86 @@ export default function Servicos() {
                         <NavServicos />
                     </div>
                     <div className={"mt-8"}>
-                        <h1>Teste</h1>
+                        <div className="relative">
+                            <div
+                                className={
+                                    "border-l-4 border-brand-orange pl-8 lg:pl-12 pb-8 text-lg md:text-xl lg:text-2xl font-normal mt-5 lg:mt-12 "
+                                }
+                            >
+                                <p
+                                    className=""
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab6.p1"),
+                                    }}
+                                ></p>
+
+                                <p
+                                    className="mt-4 lg:mt-8"
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab6.p2"),
+                                    }}
+                                ></p>
+                                <p
+                                    className="mt-4 lg:mt-8"
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab6.p3"),
+                                    }}
+                                ></p>
+                                <p
+                                    className="mt-4 lg:mt-8"
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab6.p4"),
+                                    }}
+                                ></p>
+                                <div className="float-right w-[40%] mt-8 mb-8 mr-24">
+                                    <Image
+                                        src={"/images/caixa.jpg"}
+                                        alt={
+                                            "imagem mostrando um container vindo da china com vários produtos"
+                                        }
+                                        width={674}
+                                        height={411}
+                                        layout={"responsive"}
+                                    />
+                                </div>
+                                <p
+                                    className="mt-4 lg:mt-8"
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab6.p5"),
+                                    }}
+                                ></p>
+                                <p
+                                    className="mt-4 lg:mt-8 mb-16"
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("servicos.content.tab6.p6"),
+                                    }}
+                                ></p>
+                                <NextLink
+                                    className={`inline-flex px-4 mb-24 ml-44 py-2 items-center  border rounded-xl transition-colors border-brand-blue hover:bg-brand-orange-hover text-brand-blue`}
+                                    href={{
+                                        pathname: "/contato",
+                                        query: query,
+                                    }}
+                                    as={t("nav.paths.contato")}
+                                >
+                                    <Image
+                                        className=""
+                                        src={"/icons/contact.svg"}
+                                        alt={"icone de contato pelo whatsapp"}
+                                        width={40}
+                                        height={40}
+                                        layout="fixed"
+                                    />
+                                    <p
+                                        className={
+                                            "ml-3 w-full text-center text-[16px]  sm:w-fit uppercase  "
+                                        }
+                                    >
+                                        {t("sobre.btn")}
+                                    </p>
+                                </NextLink>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
