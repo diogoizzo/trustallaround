ace6c9aec7109b68b0548b696854aaa8e264262c
diogoizzo <dizzodev@gmail.com>
2024-11-05 19:55:12 -0300

    tentando incluir hhtps

--- pages/_document.js
+++ pages/_document.js
@@ -7,25 +7,12 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
-                {/* <script
+                <script
                     type="text/javascript"
                     id="hs-script-loader"
                     async
                     defer
-                    src="//js.hs-scripts.com/47956029.js"
-                ></script> */}
-                <script
-                    type="text/javascript"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                (function(d, s, i, r) {
-                  if (d.getElementById(i)) {return;}
-                  var n = d.createElement(s), e = d.getElementsByTagName(s)[0];
-                  n.id=i;n.src='https://js.hs-scripts.com/47956029.js';
-                  e.parentNode.insertBefore(n, e);
-                })(document, 'script', 'hs-script-loader');
-              `,
-                    }}
+                    src="https://js.hs-scripts.com/47956029.js"
                 ></script>
             </body>
         </Html>
